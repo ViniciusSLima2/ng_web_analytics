fd10c15f642f7b96c86520bfedd28fc9f555017b
vslima <vinicius.slima021@gmail.com>
2025-11-06 14:08:47 -0300

    mudanca para https no backend

--- src/app/core/services/analytics.ts
+++ src/app/core/services/analytics.ts
@@ -9,7 +9,7 @@ import { forkJoin, map, Observable } from 'rxjs';
 export class Analytics {
   apiUrl = 'http://localhost:3000/';
   //newApiUrl = 'http://127.0.0.1:5000/'
-  newApiUrl = 'http://venegames21.pythonanywhere.com/'
+  newApiUrl = 'https://venegames21.pythonanywhere.com/'
   httpClient = inject(HttpClient);
 
   getSitesMetadata(){
