d765f384b1e05d492bdb7b5341666f8d7a2a5b08
vslima <vinicius.slima021@gmail.com>
2025-11-10 10:57:27 -0300

    feature: add skeletons cards em select site no loading

---
+++ src/app/feature/pages/select-site/components/skeleton-cards/skeleton-cards.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SkeletonModule } from 'primeng/skeleton';
+
+@Component({
+  selector: 'app-skeleton-cards',
+  imports: [SkeletonModule],
+  templateUrl: './skeleton-cards.html',
+  styleUrl: './skeleton-cards.css'
+})
+export class SkeletonCards {
+
+}
--- src/app/feature/pages/select-site/select-site.ts
+++ src/app/feature/pages/select-site/select-site.ts
@@ -2,23 +2,27 @@ import { Component, inject, signal } from '@angular/core';
 import { Analytics } from '../../../core/services/analytics';
 import { SiteCard } from './components/site-card/site-card';
 import { SiteMetadata } from '../../../core/models/siteMetadata.model';
+import { finalize } from 'rxjs';
+import { SkeletonCards } from "./components/skeleton-cards/skeleton-cards";
 
 @Component({
   selector: 'app-select-site',
-  imports: [SiteCard],
+  imports: [SiteCard, SkeletonCards],
   templateUrl: './select-site.html',
   styleUrl: './select-site.css'
 })
 export class SelectSite {
   protected sitesMetadata = signal<Array<SiteMetadata>>([]);
   analyticsService = inject(Analytics);
-
+  isDataLoading = signal(true);
   ngOnInit(){
     this.getAllSitesMetadata();
   }
 
   getAllSitesMetadata(){
-    this.analyticsService.getSitesMetadata().subscribe((data) => {
+    this.analyticsService.getSitesMetadata()
+    .pipe(finalize(() => this.isDataLoading.set(false)))
+    .subscribe((data) => {
       this.sitesMetadata.set(data);
     });
   }
